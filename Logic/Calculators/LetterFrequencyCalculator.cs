f8a5ac4ec1af368cd0df722ee24116eeff664d2a
Shai Aharoni <saharoni@gmail.com>
2021-07-28 22:00:58 +0300

    Day 6 - Part 1

---
+++ Logic/Calculators/LetterFrequencyCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AOC2016.Logic.Calculators
+{
+    public class LetterFrequencyCalculator
+    {
+
+        public char GetMostFrequestCharAtPoisition(string[] words, int position)
+        {
+            var charDictionary = new Dictionary<char, int>();
+
+            foreach(string word in words)
+            {
+                if (!charDictionary.ContainsKey(word[position]))
+                {
+                    charDictionary.Add(word[position], 1);
+                } else
+                {
+                    charDictionary[word[position]]++;
+                }
+            }
+
+            return charDictionary.OrderByDescending(keyVal => keyVal.Value).First().Key;
+        }
+    }
+}
---
+++ PuzzleSolvers/Day6PuzzleSolver.cs
@@ -0,0 +1,32 @@
+﻿using AOC2016.Logic.Calculators;
+using AOC2016.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2016.PuzzleSolvers
+{
+    public class Day6PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day6.txt");
+
+            var message = string.Empty;
+
+            var calculator = new LetterFrequencyCalculator();
+
+            for (int i = 0; i < inputLines[0].Length ; i++)
+            {
+                message += calculator.GetMostFrequestCharAtPoisition(inputLines, i);
+            }
+
+            return message;
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
