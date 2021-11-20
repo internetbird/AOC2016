14b2fbabd28a164b94ba3cfbb5036752994888c6
Shai Aharoni <saharoni@gmail.com>
2021-11-20 16:18:19 +0200

    Day 20

---
+++ Logic/Models/IntRange.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2016.Logic.Models
+{
+    internal class IntRange
+    {
+        public uint LowValue { get; set; }
+        public uint HighValue { get; set; }
+
+
+        public IntRange(uint lowValue, uint highValue)
+        {
+            LowValue = lowValue;
+            HighValue = highValue;
+        }
+
+        public bool Contains(uint value)
+        {
+           return LowValue <= value && HighValue >= value;
+        }
+
+        internal uint GetRangeCount()
+        {
+            return HighValue - LowValue + 1;
+        }
+    }
+}
---
+++ PuzzleSolvers/Day20PuzzleSolver.cs
@@ -0,0 +1,83 @@
+﻿using AOC2016.Logic.Models;
+using AOC2016.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2016.PuzzleSolvers
+{
+    public class Day20PuzzleSolver : IPuzzleSolver
+    {
+        List<IntRange> _intRanges;
+
+        public string SolvePuzzlePart1()
+        {
+            LoadInput();
+
+            uint minAllowedIp = 0;
+            while (!IsAllowedIP(minAllowedIp))
+            {
+                minAllowedIp++;
+            }
+
+            return minAllowedIp.ToString();
+        }
+
+        private void LoadInput()
+        {
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day20.txt");
+
+            _intRanges = ParseInput(inputLines);
+        }
+
+        private bool IsAllowedIP(uint minAllowedIp)
+        {
+            foreach (IntRange range in _intRanges)
+            {
+                if (range.Contains(minAllowedIp))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private List<IntRange> ParseInput(string[] inputLines)
+        {
+            var intRanges = new List<IntRange>();
+
+            foreach (string line in inputLines)
+            {
+                string[] rangeParts = line.Split("-");
+
+                uint lowValue = uint.Parse(rangeParts[0]);
+                uint highValue = uint.Parse(rangeParts[1]);
+
+                intRanges.Add(new IntRange(lowValue, highValue));
+            }
+            return intRanges;
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            LoadInput();
+            int numOfAllowedIPs = 0;
+
+            for (uint i = 14975795 /*Min valued allowed IP */; i <= uint.MaxValue; i++)
+            {
+                if (IsAllowedIP(i))
+                {
+                    numOfAllowedIPs++;
+                }
+
+                if (i % 100000 == 0)
+                {
+                    Console.WriteLine($"--{i}-- : Num of allowed IPs:{numOfAllowedIPs}");
+                }
+            }
+
+            return numOfAllowedIPs.ToString();
+        }
+    }
+}
