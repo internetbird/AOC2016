5ed94ad094ed7b21266144ddf0f70aba25b99102
Shai Aharoni <saharoni@gmail.com>
2022-01-03 20:50:32 +0200

    Day 23 - start

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace AOC2016
     {
         static void Main(string[] args)
         {
-            IPuzzleSolver solver = new Day22PuzzleSolver();
+            IPuzzleSolver solver = new Day23PuzzleSolver();
 
-            var solution = solver.SolvePuzzlePart2();
+            var solution = solver.SolvePuzzlePart1();
             Console.WriteLine($"The solution to the puzzle is: {solution}");
 
             Console.ReadKey();
---
+++ PuzzleSolvers/Day23PuzzleSolver.cs
@@ -0,0 +1,27 @@
+﻿using AOC;
+using AOC2016.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2016.PuzzleSolvers
+{
+    public class Day23PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            string[] programLines = InputFilesHelper.GetInputFileLines("day23.txt");
+
+            var computer = new EasterBunnyComputer();
+
+
+            return string.Empty;
+
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
