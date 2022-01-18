56b4bd10be5f64047d367e8c6c3c1c5667b1d89b
Shai Aharoni <saharoni@gmail.com>
2022-01-18 09:36:45 +0200

    Day 25 - setup

---
+++ PuzzleSolvers/Day25PuzzleSolver.cs
@@ -0,0 +1,27 @@
+﻿using AOC;
+using AOC2016.Logic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2016.PuzzleSolvers
+{
+    public class Day25PuzzleSolver : IPuzzleSolver
+    {
+        public string SolvePuzzlePart1()
+        {
+            string[] programLines = InputFilesHelper.GetInputFileLines("day25.txt");
+
+            var computer = new EasterBunnyComputer();
+            computer.LoadProgram(programLines);
+
+
+            return string.Empty;
+        }
+
+        public string SolvePuzzlePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
