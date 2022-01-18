8cf6231eb9c904b34a4bf5d07938cb05fc72a898
Shai Aharoni <saharoni@gmail.com>
2022-01-18 07:09:38 +0200

    Day 24  - Part 1

--- PuzzleSolvers/Day24PuzzleSolver.cs
+++ PuzzleSolvers/Day24PuzzleSolver.cs
@@ -1,5 +1,6 @@
 ﻿using AOC;
 using AOC2016.Logic.Models;
+using BirdLib;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -11,14 +12,60 @@ namespace AOC2016.PuzzleSolvers
     {
         public string SolvePuzzlePart1()
         {
-            string[] inputLines = InputFilesHelper.GetInputFileLines("day24.test.txt");
+            string[] inputLines = InputFilesHelper.GetInputFileLines("day24.txt");
             var map = new Map(inputLines);
+            var permutationGenerator = new PermutationGenerator();
 
-            Point point0 = map.GetPointOfInterest(0);   
-            Point point1 = map.GetPointOfInterest(4);
+            var distancesDictionary = new Dictionary<Tuple<int, int>, int>();
+            var couples = permutationGenerator.GetAllCouples(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
 
-            int shortestDistance = map.FindShortestDistanceBetween(point0, point1);
-            return shortestDistance.ToString();
+            foreach (Tuple<int, int> couple in couples)
+            {
+                var point1 = map.GetPointOfInterest(couple.Item1);
+                var point2 = map.GetPointOfInterest(couple.Item2);
+
+                int shortestDistance = map.FindShortestDistanceBetween(point1, point2);
+
+                distancesDictionary.Add(couple, shortestDistance);
+            }
+
+            int minDistance = int.MaxValue;
+            List<int[]> routes = permutationGenerator.GetAllPermutations(new[] { 1, 2, 3, 4, 5, 6, 7 });
+
+            foreach (int[] route in routes)
+            {
+                int totalDistance = CalculateTotalDistance(route.PrependItem(0), distancesDictionary);
+                if (totalDistance < minDistance)
+                {
+                    minDistance = totalDistance;
+                }
+
+            }
+
+            return minDistance.ToString();
+        }
+
+        private int CalculateTotalDistance(int[] route, Dictionary<Tuple<int, int>, int> distancesDictionary)
+        {
+            var totalDistance = 0;
+            
+            for (int i = 0; i < route.Length - 1; i++)
+            {
+                totalDistance += GetDistanceBetweenPoints(route[i], route[i + 1], distancesDictionary);
+            }
+
+            return totalDistance;
+        }
+
+        private int GetDistanceBetweenPoints(int point1Index, int point2Index, Dictionary<Tuple<int, int>, int> distancesDictionary)
+        {
+            if (distancesDictionary.ContainsKey(Tuple.Create(point1Index, point2Index)))
+            {
+                return distancesDictionary[Tuple.Create(point1Index, point2Index)];
+            } else
+            {
+                return distancesDictionary[Tuple.Create(point2Index, point1Index)];
+            }
         }
 
         public string SolvePuzzlePart2()
